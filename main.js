0a09d337a8850a2166a5fc0929ef27d9d486af4d
duc anh <11220230@st.neu.edu.vn>
2024-05-24 10:52:10 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,67 @@
+// // on tap hom qua hoc
+// let myName;
+// myName=prompt("moi cu nhap ten");
+// console.log("ten cua ban la:" +myName);
+// var soNguyen=30;
+// var soThuc=3.14;
+// console.log(typeof soNguyen);
+// console.log(typeof soThuc);
+// let diemToan;
+// console.log(diemToan);
+// //cach xuat gia tri bien
+// let soA=23;
+// let soB=24;
+// //cach 2
+// // console.log("gia bi bien", soA,"la");
+// console.log(`cah gan gia tri bien ${soA}`)
+
+// // lam bai tap theo 4 cach;
+// let a=4,b=3,ketqua=a*b;
+// console.log("ket qua cua phep tinh:" +ketqua);
+// console.log("ket qua cua phep tinh:",ketqua);
+// console.log(`ket qua cua phep tinh:${ketqua}`);
+// // console.log(`ket qua cua phep tinh %ketqua la: ketqua`);
+// console.log(`ket qua cua phep tinh %s voi %s bang %s`, a, b, ketqua);
+// cac phep toan so hoc
+// let a=10,b=9;
+// let tong=a+b;
+// let hieu=a-b;
+// let tich=a*b;
+// let thuong=a/b;
+// let soDu=a%b;
+// console.log(`tong la:${tong}`);
+// console.log(`hieu la:$s`,hieu);
+// console.log("tich la:" +tich);
+// console.log("thuong",thuong);
+// console.log(`so du:%s`,soDu)
+// bai 8 ep kieu du lieu
+// let numBer=prompt("moi cu nhap ten");
+// console.log(typeof numBer);
+// let numBer=(prompt("moi cu nhap ten"));
+// console.log(`kieu du lieu:${typeof numBer}`);
+// // toan tu gan
+// // bai tap thuc hien phep gan va in ra ket qua
+// let a=7;
+// a-=3;
+// console.log(`ket qua phep tinh:${a}`);
+// let b=15;
+// b-=6;
+// console.log(`ket qua phep tinh:${b}`);
+// let c=5;
+// c*=6;
+// console.log(`ket qua phep tinh:${c}`);
+// let e=5,f=2;
+// e-=(f+1);
+// console.log(`ket qua phep tinh:${e}`);
+// toan tu tang giam
+// let a=100;
+// a++;
+// console.log(a);
+// khai bao phuc tap
+let x=10;
+let y=20;
+
+let z=x-- - ++y +1;
+console.log(x);
+console.log(y);
+console.log(z);
